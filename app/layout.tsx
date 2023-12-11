f9c2aee8afa3b9ff1445c3f188e8fc568e19997c
Gloria <ubahgloria16@gmail.com>
2023-12-11 12:57:11 +0100

    Fixed fav icon issues

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,6 +10,9 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata: Metadata = {
   title: 'Memory Chest',
   description: 'Memory Chest is a platform dedicated to unlocking the secrets of the past, bringing historical events and documentaries to life. Our mission is to preserve and share the memories of the world, fueling curiosity and inspiring a deeper understanding of our shared history.',
+  icons: {
+    icon: "/IMG-20230830-WA0026.jpg"
+  }
 }
 
 export default function RootLayout({
